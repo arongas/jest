14c10f1e76c716f4aaecc22b2602111b4cec0403
Alex Rongas <ext-332@cpp.canon>
2021-02-11 09:14:29 +0200

    Setup and Teardown methods

--- __tests__/maths.spec.js
+++ __tests__/maths.spec.js
@@ -1,9 +1,31 @@
 const sum = require('../src/maths');
 
 describe("Math library tests", () => {
+  var database;
+  var cnt =0;
+  beforeAll(() => {
+    database=[];
+  });
+
+  afterAll(() => {
+    database=null
+  });
+
+  beforeEach(() => {
+    database.push(++cnt);
+  });
+
+  afterEach(() => {
+    console.log("Finished test: "+database[database.length-1])
+  });
+
 
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+  });
+
+  it('adds -2 + 2 to equal 3', () => {
+    expect(sum(-2, 2)).toBe(0);
   });
 
 });
